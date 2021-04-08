d620d27ba6930bae950d4755aed0cc614780cbeb
Uugne <ugne.kurkyte@gmail.com>
2021-04-08 17:19:54 +0300

    new

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-console.log('Labas');
+const selector = '.calc';
+const DOM = document.querySelector(selector);
+
+const screen = document.querySelector('.screen');
+screen.innerText = 300;
+
+console.log(screen);
+
